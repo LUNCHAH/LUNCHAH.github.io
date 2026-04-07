645e518aa5f800542c9ed60a887c60c65502ac02
LUNCHAH <2135537336@qq.com>
2026-04-07 17:55:29 +0800

    Site updated: 2026-04-07 17:55:29

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1775553728928";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1775555722365";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
